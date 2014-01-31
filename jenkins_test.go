f191aefb0463e747844cd58a7d4be0d2ce16ea66
Chavez <nailsbrokeandfell@gmail.com>
2014-01-30 21:40:57 -0800

    Add ginkgo and update tests

--- jenkins_test.go
+++ jenkins_test.go
@@ -1,13 +1,42 @@
 package jenkins
 
 import (
-	"testing"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"hash"
 )
 
-func TestHash(t *testing.T) {
-	result := Hash([]byte("Apple"))
-	var expected uint32 = 884782484
-	if result != expected {
-		t.Errorf("Hash expected %v but got %v", expected, result)
-	}
-}
+var _ = Describe("Jenkins", func() {
+
+	Describe("New", func() {
+
+		It("returns jenkhash", func() {
+			var h *jenkhash
+			hash := New()
+			Expect(hash).To(BeAssignableToTypeOf(h))
+		})
+	})
+
+	Describe("Write", func() {
+
+		var jhash hash.Hash32
+		var key []byte
+
+		BeforeEach(func() {
+			jhash = New()
+			key = []byte("Apple")
+		})
+
+		It("returns key length", func() {
+			length, _ := jhash.Write(key)
+			Expect(length).To(Equal(5))
+		})
+
+		It("has no error", func() {
+			_, err := jhash.Write(key)
+			Expect(err).To(BeNil())
+		})
+
+	})
+
+})
